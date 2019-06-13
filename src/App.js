f7dc0b6b6a336a7bd30ca9edeb42fe1cdf941fd6
tsalm0n <tsalm0n@twc.com>
2019-06-13 13:12:04 -0400

    begin developing deck shuffle functionality &  add package.json, etc.

--- src/App.js
+++ src/App.js
@@ -6,17 +6,32 @@ import './App.css';
 import Component1 from './Component1/Component1';
 
 class App extends Component {
+
+  state = {
+    deckArray: [
+      {cardName: 'The Magician', descUp: 'Magician description for upright card'},
+      {cardName: 'The High Priestess', descUp: 'High Priestess description for upright card'},
+      {cardName: 'The Empress', descUp: 'Empress description for upright card'},
+    ]
+  }
+
+  deckShuffleHandler = ()=>{
+    console.log('button clicked');
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React Tarot App</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Component1 prop1='some property' prop2='another property'>This is a child of Component1</Component1>
+        <button onClick={this.deckShuffleHandler}>shuffle</button>
+        {/* <Component1 prop1='some property' prop2='another property'>This is a child of Component1</Component1>
+        <Component1 prop1='some property' prop2='another property'>This is a child of Component1</Component1> */}
+        <Component1 crdName={this.state.deckArray[0].cardName}>Card Meaning: {this.state.deckArray[0].descUp}</Component1>
+        <Component1 crdName={this.state.deckArray[1].cardName}>Card Meaning: {this.state.deckArray[1].descUp}</Component1>
+        <Component1 crdName={this.state.deckArray[2].cardName}>Card Meaning: {this.state.deckArray[2].descUp}</Component1>
       </div>
     );
   }
--- src/Component1/Component1.js
+++ src/Component1/Component1.js
@@ -3,9 +3,12 @@ import React from 'react';
 const component1 = (props)=>{
     return (
         <div>
-            <h3>here's some Component1 JSX</h3>
+            {/* <h3>here's some Component1 JSX</h3>
             <p>this is prop1 of Component1: {props.prop1}</p>
             <p>this is prop2 of Component1: {props.prop2}</p>
+            <p>{props.children}</p>
+            <p>here is some dynamic content ==> {Math.floor(Math.random()*10)}</p> */}
+            <h3>{props.crdName}</h3>
             <p>{props.children}</p>
         </div>
         
